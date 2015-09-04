887ffd007d1da9e56a5302e077708035cbfe0fbe
befegimot <wprsgm@yahoo.co.jp>
2015-09-04 15:58:29 +0900

    9/4
    型などがエラーの原因と仮定し検証中

--- MeidaiTest.R
+++ MeidaiTest.R
@@ -3,9 +3,10 @@ library(data.table)
 setwd("~/R/data")
 old.op <- options(max.print=999999)
 meidai <- read.csv("Driving_data_list_20150418_editHashimoto.csv", header=FALSE)
-feature <- as.matrix(meidai[1:70, 5:13])
-answer <- as.numeric(meidai$V14[1:70])
-Mdata <- cbind(feature, answer)
+feature <- as.matrix(meidai[1:70, 5:7])
+answer <- as.logical(meidai$V14[1:70])
+MdataMT <- cbind(answer, feature)
+Mdata <- as.data.frame(MdataMT)
 result <- svm(feature, answer, type="C-classification", cost=10, kernel="linear", scale=TRUE, trelance=1e-5)
 
 #meidai.svm <- svm(meidai[1:70,14]~., data = meidai)
@@ -13,13 +14,11 @@ result <- svm(feature, answer, type="C-classification", cost=10, kernel="linear"
 
 #svm <- svm(~., data=meidai[feature,])
 
-x <- subset(answer, select = -V1)
-y <- meidai$V14[1:70]
-model <- svm(x, y)
-model <- result
 
 pred <- predict(result,feature)
 pred <- fitted(result)
 table(pred, answer)
-pred <- predict(model, feature, decision.values = TRUE)
+pred <- predict(result, feature, decision.values = TRUE)
 attr(pred, "decision.values")[1:2,] #?[1:2,]‚Å‚æ‚¢H
+
+plot(result, Mdata, pred~.)
